280d338f8042e646e11c045192a1a751d6057db3
Lukas <lbo3144@students.fhv.at>
2022-03-23 17:58:06 +0100

    Changed interface, removed allSoundCarriers

--- src/main/java/at/fhv/teame/sharedlib/rmi/SoundCarrierService.java
+++ src/main/java/at/fhv/teame/sharedlib/rmi/SoundCarrierService.java
@@ -7,9 +7,9 @@ import java.rmi.RemoteException;
 import java.util.List;
 
 public interface SoundCarrierService extends Remote {
-    List<SoundCarrierDTO> allSoundCarriers() throws RemoteException;
-
     List<SoundCarrierDTO> soundCarriersByAlbumName(String album, int pageNr) throws RemoteException;
 
     List<SoundCarrierDTO> soundCarriersByArtistName(String artist, int pageNr) throws RemoteException;
+
+    List<SoundCarrierDTO> soundCarriersBySongName(String album, int pageNr) throws RemoteException;
 }
