73473e02c09d6b21f6a817744f81fb347382837d
Ali Cinar <ali.cinar@outlook.at>
2022-04-01 20:22:52 +0200

    Removed CreateInvoiceService.java to RMIFactory.java. Changed confirmPurchase parameters.
    Co-authored-by: Zikrihan <zikrihan.sahin@students.fhv.at>

--- lib/src/main/java/at/fhv/teame/sharedlib/rmi/CreateInvoiceService.java
+++
@@ -1,8 +0,0 @@
-package at.fhv.teame.sharedlib.rmi;
-
-import java.rmi.Remote;
-import java.util.Map;
-
-public interface CreateInvoiceService extends Remote {
-    void createInvoice(Map<String, Integer> shoppingCartItems);
-}
--- lib/src/main/java/at/fhv/teame/sharedlib/rmi/PurchaseSoundCarrierService.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/rmi/PurchaseSoundCarrierService.java
@@ -2,11 +2,12 @@ package at.fhv.teame.sharedlib.rmi;
 
 import at.fhv.teame.sharedlib.rmi.exceptions.PurchaseFailedException;
 
+import java.math.BigDecimal;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.Map;
 
 public interface PurchaseSoundCarrierService extends Remote {
 
-    void confirmPurchase(Map<String, Integer> shoppingCartItems, String paymentMethod) throws PurchaseFailedException, RemoteException;
+    void confirmPurchase(Map<String, Integer> shoppingCartItems, String paymentMethod, BigDecimal totalPrice) throws PurchaseFailedException, RemoteException;
 }
