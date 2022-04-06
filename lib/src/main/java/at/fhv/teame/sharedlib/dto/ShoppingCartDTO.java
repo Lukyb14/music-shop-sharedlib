661fffe36ad13d410a49742568bbec94431797e4
Ali Cinar <ali.cinar@outlook.at>
2022-04-07 00:33:57 +0200

    Adjusted ShoppingCartDTO
    Co-authored-by: Zikrihan <zikrihan.sahin@students.fhv.at>

--- lib/src/main/java/at/fhv/teame/sharedlib/dto/ShoppingCartDTO.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/dto/ShoppingCartDTO.java
@@ -1,62 +1,44 @@
 package at.fhv.teame.sharedlib.dto;
 
 import java.io.Serializable;
+import java.util.Map;
 import java.util.Objects;
-
 
 public class ShoppingCartDTO implements Serializable {
     private static final long serialVersionUID = 6529685098267757690L;
 
-
-    private String articleId;
-    private String albumName;
-    private String artist;
-    private int nrOfSongs;
-    private String medium;
-    private int amount;
-    private int stock;
-    private String price;
-
+    private Map<String, Integer> purchasedItems;
+    private String paymentMethod;
+    private String customerFirstName;
+    private String customerLastName;
+    private String customerAddress;
 
     public static Builder builder() {
         return new Builder();
     }
 
-    public String getArticleId() {
-        return articleId;
+    public Map<String, Integer> getPurchasedItems() {
+        return purchasedItems;
     }
 
-    public String getAlbumName() {
-        return albumName;
+    public String getPaymentMethod() {
+        return paymentMethod;
     }
 
-    public String getArtist() {
-        return artist;
+    public String getCustomerFirstName() {
+        return customerFirstName;
     }
 
-    public int getNrOfSongs() {
-        return nrOfSongs;
+    public String getCustomerLastName() {
+        return customerLastName;
     }
 
-    public String getMedium() {
-        return medium;
+    public String getCustomerAddress() {
+        return customerAddress;
     }
 
-    public int getAmount() {
-        return amount;
+    private ShoppingCartDTO() {
     }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-
-    private ShoppingCartDTO() {}
-
 
     public static class Builder {
         private final ShoppingCartDTO instance;
@@ -66,58 +48,46 @@ public class ShoppingCartDTO implements Serializable {
 
         }
 
-
-        private Builder withShoppingCartEntity(String articleId, String albumName, String artist,
-                                               int nrOfSongs, String medium, int amount,
-                                               int stock, String price) {
-            this.instance.articleId = articleId;
-            this.instance.albumName = albumName;
-            this.instance.artist = artist;
-            this.instance.nrOfSongs = nrOfSongs;
-            this.instance.medium = medium;
-            this.instance.amount = amount;
-            this.instance.stock = stock;
-            this.instance.price = price;
+        private Builder withShoppingCartEntity(Map<String, Integer> purchasedItems,
+                                               String paymentMethod,
+                                               String customerFirstName,
+                                               String customerLastName,
+                                               String customerAddress
+        ) {
+            this.instance.purchasedItems = purchasedItems;
+            this.instance.paymentMethod = paymentMethod;
+            this.instance.customerFirstName = customerFirstName;
+            this.instance.customerLastName = customerLastName;
+            this.instance.customerAddress = customerAddress;
             return this;
-
         }
 
-
-
-
         public ShoppingCartDTO build() {
-            Objects.requireNonNull(this.instance.articleId, "articleId must be set in ShoppingCartDTO");
-            Objects.requireNonNull(this.instance.albumName, "albumName must be set in ShoppingCartDTO");
-            Objects.requireNonNull(this.instance.artist, "artist must be set in ShoppingCartDTO");
-            Objects.requireNonNull(this.instance.nrOfSongs, "nrOfSongs must be set in ShoppingCartDTO");
-            Objects.requireNonNull(this.instance.medium, "medium must be set in ShoppingCartDTO");
-            Objects.requireNonNull(this.instance.stock, "stock must be set in ShoppingCartDTO");
-            Objects.requireNonNull(this.instance.price, "price must be set in ShoppingCartDTO");
+            Objects.requireNonNull(this.instance.purchasedItems, "purchasedItems must be set in ShoppingCartDTO");
+            Objects.requireNonNull(this.instance.paymentMethod, "paymentMethod must be set in ShoppingCartDTO");
+            Objects.requireNonNull(this.instance.customerFirstName, "customerFirstName must be set in ShoppingCartDTO");
+            Objects.requireNonNull(this.instance.customerLastName, "customerLastName must be set in ShoppingCartDTO");
+            Objects.requireNonNull(this.instance.customerAddress, "customerAddress must be set in ShoppingCartDTO");
 
             return this.instance;
         }
-
-
     }
 
     @Override
-    public boolean equals (Object o) {
+    public boolean equals(Object o) {
         if (this == o) return true;
-        if(o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         ShoppingCartDTO that = (ShoppingCartDTO) o;
-        return Objects.equals(articleId, that.articleId)
-                && Objects.equals(albumName, that.albumName)
-                && Objects.equals(artist, that.artist)
-                && Objects.equals(nrOfSongs, that.nrOfSongs)
-                && Objects.equals(medium, that.medium)
-                && Objects.equals(amount, that.amount)
-                && Objects.equals(stock, that.stock)
-                && Objects.equals(price, that.price);
+        return Objects.equals(purchasedItems, that.purchasedItems)
+                && Objects.equals(paymentMethod, that.paymentMethod)
+                && Objects.equals(customerFirstName, that.customerFirstName)
+                && Objects.equals(customerLastName, that.customerLastName)
+                && Objects.equals(customerAddress, that.customerAddress);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(articleId, albumName, artist, nrOfSongs, medium,
-                amount, stock, price);
+        return Objects.hash(purchasedItems, paymentMethod, customerFirstName, customerLastName, customerAddress);
     }
 }
+
