8f083709502cc41e4540e0d3930b3702215dde63
Zikrihan <93253402+Zikrihan@users.noreply.github.com>
2022-04-06 14:59:05 +0200

    Edited shopping Cart DTO
    Co-authored-by: Ali <ali.cinar@outlook.at>

--- lib/src/main/java/at/fhv/teame/sharedlib/dto/ShoppingCartDTO.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/dto/ShoppingCartDTO.java
@@ -83,19 +83,6 @@ public class ShoppingCartDTO implements Serializable {
         }
 
 
-        @Override
-        public boolean equals (Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Builder builder = (Builder) o;
-            return Objects.equals(instance, builder.instance);
-        }
-
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(instance);
-        }
 
 
         public ShoppingCartDTO build() {
