86455db7c84b27889cb979ee6238e430767c49fd
Zikrihan <93253402+Zikrihan@users.noreply.github.com>
2022-04-08 23:39:53 +0200

    Edited InvoiceDTO
    Co-authored-by: Ali <ali.cinar@outlook.at>
    Co-authored-by: Lukas <lbo3144@students.fhv.at>
    Co-authored-by: Halim <halimukajev@gmail.com>

--- lib/src/main/java/at/fhv/teame/sharedlib/dto/InvoiceLineDTO.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/dto/InvoiceLineDTO.java
@@ -11,7 +11,7 @@ public class InvoiceLineDTO implements Serializable {
     private String album;
     private String medium;
     private int quantity;
-    private int amountOfReturnableItems;
+
     private int amountOfReturnedItems;
     private String price;
 
@@ -39,10 +39,6 @@ public class InvoiceLineDTO implements Serializable {
         return quantity;
     }
 
-    public int getAmountOfReturnableItems() {
-        return amountOfReturnableItems;
-    }
-
     public int getAmountOfReturnedItems() {
         return amountOfReturnedItems;
     }
@@ -63,15 +59,14 @@ public class InvoiceLineDTO implements Serializable {
         }
 
         public Builder withInvoiceLineEntity(String articleId, String artistName, String album,
-                                       String medium, int quantity, int amountOfReturnableItem,
-                                       int amountOfReturnedItem, String price
+                                             String medium, int quantity,
+                                             int amountOfReturnedItem, String price
         ) {
             this.instance.articleId = articleId;
             this.instance.artistName = artistName;
             this.instance.album = album;
             this.instance.medium = medium;
             this.instance.quantity = quantity;
-            this.instance.amountOfReturnableItems = amountOfReturnableItem;
             this.instance.amountOfReturnedItems = amountOfReturnedItem;
             this.instance.price = price;
             return this;
@@ -94,7 +89,6 @@ public class InvoiceLineDTO implements Serializable {
         if (o == null || getClass() != o.getClass()) return false;
         InvoiceLineDTO that = (InvoiceLineDTO) o;
         return quantity == that.quantity
-                && amountOfReturnableItems == that.amountOfReturnableItems
                 && amountOfReturnedItems == that.amountOfReturnedItems
                 && Objects.equals(articleId, that.articleId)
                 && Objects.equals(artistName, that.artistName)
@@ -106,6 +100,6 @@ public class InvoiceLineDTO implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(articleId, artistName, album, medium, quantity,
-                amountOfReturnableItems, amountOfReturnedItems, price);
+                amountOfReturnedItems, price);
     }
 }
