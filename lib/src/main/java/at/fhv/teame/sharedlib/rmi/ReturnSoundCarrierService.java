e00c68e8cce26cc50a8631277c08181e898e0835
Ali Cinar <ali.cinar@outlook.at>
2022-04-10 23:35:38 +0200

    Added Exceptions
    Co-authored-by: Zikrihan <zikrihan.sahin@students.fhv.at>
    Co-authored-by: Umut <umut.caglayan@students.fhv.at>
    Co-authored-by: Hüseyin <hueseyin.arziman@gmail.com>
    Co-authored-by: Lukas <lbo3144@students.fhv.at>
    Co-authored-by: Yusuf <yce5586@students.fhv.at>
    Co-authored-by: Halim <halimukajev@gmail.com>

--- lib/src/main/java/at/fhv/teame/sharedlib/rmi/ReturnSoundCarrierService.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/rmi/ReturnSoundCarrierService.java
@@ -1,9 +1,11 @@
 package at.fhv.teame.sharedlib.rmi;
 
 import at.fhv.teame.sharedlib.dto.WithdrawalDTO;
+import at.fhv.teame.sharedlib.rmi.exceptions.WithdrawalFailedException;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface ReturnSoundCarrierService extends Remote {
-    public void withDrawSoundCarrier(WithdrawalDTO withdrawalDTO);
+    public void withDrawSoundCarrier(WithdrawalDTO withdrawalDTO) throws WithdrawalFailedException, RemoteException;
 }
