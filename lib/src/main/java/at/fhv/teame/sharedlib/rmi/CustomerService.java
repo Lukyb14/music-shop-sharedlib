b25b1f9fb2ea1ae61763157f2c55235d4eebb6a1
Lukas <lbo3144@students.fhv.at>
2022-04-11 21:48:21 +0200

    Added CustomerService for customer-backend as a replacement for SearchCustomerService

---
+++ lib/src/main/java/at/fhv/teame/sharedlib/rmi/CustomerService.java
@@ -0,0 +1,17 @@
+package at.fhv.teame.sharedlib.rmi;
+
+import at.fhv.teame.sharedlib.dto.CustomerDTO;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface CustomerService extends Remote {
+    List<CustomerDTO> getCustomerByFullName(String givenName, String familyName, int pageNr) throws RemoteException;
+
+    List<CustomerDTO> getCustomerByFamilyName(String familyName, int pageNr) throws RemoteException;
+
+    int totResultsByFullName(String givenName, String familyName) throws RemoteException;
+
+    int totResultsByFamilyName(String familyName) throws RemoteException;
+}
