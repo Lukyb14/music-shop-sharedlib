e3d4ebeb3850106ca070170bdbbcb08910e3a38a
uc38 <umut.caglayan@students.fhv.at>
2022-04-12 17:14:06 +0200

    added durtation to songDTO

--- lib/src/main/java/at/fhv/teame/sharedlib/dto/SongDTO.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/dto/SongDTO.java
@@ -8,13 +8,17 @@ public class SongDTO implements Serializable {
 
     private String songName;
     private String release;
-
+    private String duration;
     public String getSongName() {
         return songName;
     }
 
     public String getRelease() {
         return release;
+    }
+
+    public String getDuration() {
+        return duration;
     }
 
     public static Builder builder() {
@@ -30,9 +34,10 @@ public class SongDTO implements Serializable {
             this.instance = new SongDTO();
         }
 
-        public Builder withSongEntity(String title, String release) {
+        public Builder withSongEntity(String title, String release, String duration) {
             this.instance.songName = title;
             this.instance.release = release;
+            this.instance.duration = duration;
 
             return this;
         }
@@ -40,6 +45,8 @@ public class SongDTO implements Serializable {
         public SongDTO build(){
             Objects.requireNonNull(this.instance.songName, "songName must be set in SoundCarrierDTO");
             Objects.requireNonNull(this.instance.release, "release must be set in SoundCarrierDTO");
+            Objects.requireNonNull(this.instance.duration, "duration must be set in SoundCarrierDTO");
+
             return this.instance;
         }
     }
@@ -49,11 +56,11 @@ public class SongDTO implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SongDTO songDTO = (SongDTO) o;
-        return Objects.equals(songName, songDTO.songName) && Objects.equals(release, songDTO.release);
+        return Objects.equals(songName, songDTO.songName) && Objects.equals(release, songDTO.release) && Objects.equals(duration, songDTO.duration);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(songName, release);
+        return Objects.hash(songName, release, duration);
     }
 }
