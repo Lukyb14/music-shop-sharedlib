42bca55815f7c5ebfefd2ffbb8722ac9fcc431cf
yce5586 <yce5586@students.fhv.at>
2022-04-15 17:32:55 +0200

    MessageService method allTopics

--- lib/src/main/java/at/fhv/teame/sharedlib/rmi/MessageService.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/rmi/MessageService.java
@@ -3,10 +3,13 @@ package at.fhv.teame.sharedlib.rmi;
 import at.fhv.teame.sharedlib.dto.PublishMessageDTO;
 import at.fhv.teame.sharedlib.rmi.exceptions.PublishingFailedException;
 
-import javax.naming.NamingException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface MessageService extends Remote {
     void publishMessage(PublishMessageDTO publishMessageDTO, String sessionId) throws RemoteException, PublishingFailedException;
+
+    List<String> allTopics();
 }
+
