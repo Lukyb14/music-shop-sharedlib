71e4a85c673047d393f0362738bc9a48e14770fc
Hüseyin Arziman <hueseyin.arziman@gmail.com>
2022-04-15 10:33:09 +0200

    removed JMSException

--- lib/src/main/java/at/fhv/teame/sharedlib/rmi/MessageService.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/rmi/MessageService.java
@@ -2,11 +2,10 @@ package at.fhv.teame.sharedlib.rmi;
 
 import at.fhv.teame.sharedlib.dto.PublishMessageDTO;
 
-import javax.jms.JMSException;
 import javax.naming.NamingException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface MessageService extends Remote {
-    void publishMessage(PublishMessageDTO publishMessageDTO, String sessionId) throws RemoteException, NamingException, JMSException;
+    void publishMessage(PublishMessageDTO publishMessageDTO, String sessionId) throws RemoteException, NamingException;
 }
