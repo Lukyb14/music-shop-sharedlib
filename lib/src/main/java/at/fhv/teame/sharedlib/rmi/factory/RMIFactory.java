17b312d51f1879362f765c7ae7bbef1646234250
yce5586 <yce5586@students.fhv.at>
2022-04-15 12:20:31 +0200

    RMIFactory implement createMessageServiceImpl

--- lib/src/main/java/at/fhv/teame/sharedlib/rmi/factory/RMIFactory.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/rmi/factory/RMIFactory.java
@@ -17,4 +17,6 @@ public interface RMIFactory extends Remote {
     AuthenticationService createSearchAuthenticationServiceImpl() throws RemoteException;
 
     WithdrawSoundCarrierService createWithdrawSoundCarrierServiceImpl() throws RemoteException;
+
+    MessageService createMessageServiceImpl() throws RemoteException;
 }
