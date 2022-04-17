a4b48394be9c82ef2b89e8c8864bafeb179ae853
Lukas <lbo3144@students.fhv.at>
2022-04-17 19:04:21 +0200

    Added ReceiveMessageDTO

---
+++ lib/src/main/java/at/fhv/teame/sharedlib/dto/ReceiveMessageDTO.java
@@ -0,0 +1,34 @@
+package at.fhv.teame.sharedlib.dto;
+
+import java.io.Serializable;
+
+public class ReceiveMessageDTO implements Serializable {
+    private static final long serialVersionUID = 6529685098267757690L;
+    private final String topic;
+    private final String title;
+    private final String content;
+    private final long timestamp;
+
+    public ReceiveMessageDTO(String topic, String title, String content, long timestamp) {
+        this.topic = topic;
+        this.title = title;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public String getTopic() {
+        return this.topic;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
--- lib/src/main/java/at/fhv/teame/sharedlib/rmi/MessageService.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/rmi/MessageService.java
@@ -1,15 +1,20 @@
 package at.fhv.teame.sharedlib.rmi;
 
 import at.fhv.teame.sharedlib.dto.PublishMessageDTO;
+import at.fhv.teame.sharedlib.dto.ReceiveMessageDTO;
+import at.fhv.teame.sharedlib.rmi.exceptions.InvalidSessionException;
 import at.fhv.teame.sharedlib.rmi.exceptions.PublishingFailedException;
+import at.fhv.teame.sharedlib.rmi.exceptions.ReceiveFailedException;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
 public interface MessageService extends Remote {
-    void publishMessage(PublishMessageDTO publishMessageDTO, String sessionId) throws RemoteException, PublishingFailedException;
+    void publishMessage(PublishMessageDTO publishMessageDTO, String sessionId) throws RemoteException, PublishingFailedException, InvalidSessionException;
 
-    List<String> allTopics() throws RemoteException;
+    List<ReceiveMessageDTO> fetchMessages(String sessionId) throws ReceiveFailedException, InvalidSessionException;
+
+    List<String> allTopics(String sessionId) throws RemoteException, InvalidSessionException;
 }
 
