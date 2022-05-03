7c957cf3deb6f14e951ad8866b530972421f92bd
Lukas <lbo3144@students.fhv.at>
2022-05-03 22:55:50 +0200

    small fix

--- lib/src/main/java/at/fhv/teame/sharedlib/ejb/AuthenticationServiceRemote.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/ejb/AuthenticationServiceRemote.java
@@ -3,7 +3,6 @@ package at.fhv.teame.sharedlib.ejb;
 import at.fhv.teame.sharedlib.dto.SessionDTO;
 import at.fhv.teame.sharedlib.exceptions.InvalidSessionException;
 import at.fhv.teame.sharedlib.exceptions.LoginFailedException;
-import at.fhv.teame.sharedlib.rmi.LoggedInClient;
 
 import javax.ejb.Remote;
 
@@ -12,6 +11,6 @@ public interface AuthenticationServiceRemote {
     SessionDTO login(String username, String password) throws LoginFailedException;
     void logout(String sessionId);
 
-    void rememberClient(LoggedInClient loggedInClient, String sessionId) throws InvalidSessionException;
+    void rememberClient(LoggedInClientRemote loggedInClient, String sessionId) throws InvalidSessionException;
 
 }
