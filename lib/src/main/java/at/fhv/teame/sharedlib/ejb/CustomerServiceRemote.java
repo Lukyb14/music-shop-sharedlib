3155dfa6063cbb4370be26454a6cac009305fab8
Hüseyin Arziman <hueseyin.arziman@gmail.com>
2022-05-04 18:17:48 +0200

    changed interface in CustomerServiceRemote

--- lib/src/main/java/at/fhv/teame/sharedlib/ejb/CustomerServiceRemote.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/ejb/CustomerServiceRemote.java
@@ -1,17 +1,19 @@
 package at.fhv.teame.sharedlib.ejb;
 
 import at.fhv.teame.sharedlib.dto.CustomerDTO;
+import at.fhv.teame.sharedlib.exceptions.InvalidSessionException;
 
 import javax.ejb.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
 @Remote
 public interface CustomerServiceRemote {
-    List<CustomerDTO> getCustomerByFullName(String givenName, String familyName, int pageNr);
+    List<CustomerDTO> getCustomerByFullName(String givenName, String familyName, int pageNr, String sessionId) throws InvalidSessionException;
 
-    List<CustomerDTO> getCustomerByFamilyName(String familyName, int pageNr);
+    List<CustomerDTO> getCustomerByFamilyName(String familyName, int pageNr, String sessionId) throws InvalidSessionException;
 
-    int totResultsByFullName(String givenName, String familyName);
+    int totResultsByFullName(String givenName, String familyName, String sessionId) throws InvalidSessionException;
 
-    int totResultsByFamilyName(String familyName);
+    int totResultsByFamilyName(String familyName, String sessionId) throws InvalidSessionException;
 }
