b3df2b44fb001340f8ead05b1cbb63a22222f7ea
Hüseyin Arziman <hueseyin.arziman@gmail.com>
2022-05-04 18:28:37 +0200

    updated CustomerServiceRemote

--- lib/src/main/java/at/fhv/teame/sharedlib/ejb/CustomerServiceRemote.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/ejb/CustomerServiceRemote.java
@@ -1,19 +1,17 @@
 package at.fhv.teame.sharedlib.ejb;
 
 import at.fhv.teame.sharedlib.dto.CustomerDTO;
-import at.fhv.teame.sharedlib.exceptions.InvalidSessionException;
-
 import javax.ejb.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
 @Remote
 public interface CustomerServiceRemote {
-    List<CustomerDTO> getCustomerByFullName(String givenName, String familyName, int pageNr, String sessionId) throws InvalidSessionException;
+    List<CustomerDTO> getCustomerByFullName(String givenName, String familyName, int pageNr) throws RemoteException;
 
-    List<CustomerDTO> getCustomerByFamilyName(String familyName, int pageNr, String sessionId) throws InvalidSessionException;
+    List<CustomerDTO> getCustomerByFamilyName(String familyName, int pageNr) throws RemoteException;
 
-    int totResultsByFullName(String givenName, String familyName, String sessionId) throws InvalidSessionException;
+    int totResultsByFullName(String givenName, String familyName) throws RemoteException;
 
-    int totResultsByFamilyName(String familyName, String sessionId) throws InvalidSessionException;
+    int totResultsByFamilyName(String familyName) throws RemoteException;
 }
