84c19c9485c5ea1ddbb6c70f4c3546b138f1e6d0
Hüseyin Arziman <hueseyin.arziman@gmail.com>
2022-05-12 07:56:24 +0200

    remote annotation

--- lib/src/main/java/at/fhv/teame/sharedlib/ejb/SessionValidationRemote.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/ejb/SessionValidationRemote.java
@@ -2,6 +2,9 @@ package at.fhv.teame.sharedlib.ejb;
 
 import at.fhv.teame.sharedlib.exceptions.InvalidSessionException;
 
+import javax.ejb.Remote;
+
+@Remote
 public interface SessionValidationRemote {
     void validateAny(String sessionId) throws InvalidSessionException;
 
