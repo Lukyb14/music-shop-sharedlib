d1e687ae4aebd7f5ded8fb32432606f5d76c6668
Hüseyin Arziman <hueseyin.arziman@gmail.com>
2022-06-02 12:12:54 +0200

    purchasedFailedException

--- lib/src/main/java/at/fhv/teame/sharedlib/ejb/PurchaseDigitalSongServiceRemote.java
+++ lib/src/main/java/at/fhv/teame/sharedlib/ejb/PurchaseDigitalSongServiceRemote.java
@@ -1,6 +1,7 @@
 package at.fhv.teame.sharedlib.ejb;
 
 import at.fhv.teame.sharedlib.exceptions.InvalidCredentialsException;
+import at.fhv.teame.sharedlib.exceptions.PurchaseFailedException;
 
 import javax.ejb.Remote;
 import java.util.List;
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Remote
 public interface PurchaseDigitalSongServiceRemote {
-    void purchaseSong(List<String> purchasedSongs, String email, String cvc) throws InvalidCredentialsException;
+    void purchaseSong(List<String> purchasedSongs, String email, String cvc) throws InvalidCredentialsException, PurchaseFailedException;
 }
